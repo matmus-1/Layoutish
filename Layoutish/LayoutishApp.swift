1a3fe7cb081c89ce27cd94f5f97e096469055b3b
matmus-1 <matmus1@pm.me>
2026-01-30 10:10:51 +0000

    Initial Commit

---
+++ Layoutish/LayoutishApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutishApp.swift
+//  Layoutish
+//
+//  Created by Ross Mckinlay on 30/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct LayoutishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
